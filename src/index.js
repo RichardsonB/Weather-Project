5fa7adf3395dc09887c3b60138c21e8d612d4133
RichardsonB <54607787+RichardsonB@users.noreply.github.com>
2021-06-16 10:56:40 -0400

    Added weather description

--- src/index.js
+++ src/index.js
@@ -28,11 +28,17 @@ function showWeather(response) {
       "src",
       `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
     );
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].description;
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
 }
 
 function searchCity(city) {
   let apiKey = "339c8736b2fa0090f72dc5d2b1173d91";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(showWeather);
 }
 
